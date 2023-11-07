33ee286ddaf5e1445ea1bbab79e0f4a32c5e1ac6
Louis <1659035538@qq.com>
2023-11-07 21:52:30 +0800

    add: 5&6

--- cpp/src/1_05_矩阵稀松扫描.cpp
+++ cpp/src/1_05_矩阵稀松扫描.cpp
@@ -1,7 +1,83 @@
+#include "dbg.h"
+
+#include <cassert>
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
+pair<int, int> getSparseInfo(int m, int n, vector<vector<int>> &matrix) {
+    int         sparseRows = 0, sparseCols = 0;
+    vector<int> M(m), N(n);
+    for (int i = 0; i < m; ++i) {
+        for (int j = 0; j < n; ++j) {
+            if (matrix[i][j] == 0) {
+                M[i]++;
+                N[j]++;
+                // dbg(i, j, M, N);
+            }
+        }
+        if (M[i] >= n / 2) { // 这样更高效，可读性下降，但方便调试
+            sparseRows++;
+            // dbg(sparseRows, m);
+        }
+    }
+
+    for (int j = 0; j < n; ++j) {
+        if (N[j] >= m / 2) {
+            sparseCols++;
+        }
+    }
+
+    return {sparseRows, sparseCols};
+}
+
+void test() {
+    {
+        vector<vector<int>> matrix    = {{-1, 0, 1}, {0, 0, 0}, {-1, 0, 0}, {0, -1, 0}, {0, 0, 0}};
+        auto [sparseRows, sparseCols] = getSparseInfo(5, 3, matrix);
+        assert(sparseRows == 5);
+        assert(sparseCols == 3);
+    }
+
+    {
+        vector<vector<int>> matrix    = {{1, 0, 0}, {0, 0, 1}, {1, 1, 1}};
+        auto [sparseRows, sparseCols] = getSparseInfo(3, 3, matrix);
+        assert(sparseRows == 2);
+        assert(sparseCols == 3);
+    }
+
+    {
+        vector<vector<int>> matrix    = {{1, 1, 1}, {1, 1, 1}, {1, 1, 1}};
+        auto [sparseRows, sparseCols] = getSparseInfo(3, 3, matrix);
+        assert(sparseRows == 0);
+        assert(sparseCols == 0);
+    }
+
+    cout << "All test cases passed!" << endl;
+}
+
 int main() {
-    
+    test();
+
+    int m, n;
+    cout << "Enter m x n" << endl;
+    cin >> m >> n;
+
+    if (m != 0 && n != 0) {
+        cout << "Enter the matrix: " << endl;
+    }
+
+    vector<vector<int>> matrix(m, vector<int>(n));
+    for (int i = 0; i < m; ++i) {
+        for (int j = 0; j < n; ++j) {
+            cin >> matrix[i][j];
+        }
+    }
+
+    cout << "Result: " << endl;
+    auto [sparseRows, sparseCols] = getSparseInfo(m, n, matrix);
+    cout << "Sparse Rows: " << sparseRows << endl;
+    cout << "Sparse Cols: " << sparseCols << endl;
+    return 0;
 }
--- cpp/src/1_06_阿里巴巴找黄金宝箱I.cpp
+++ cpp/src/1_06_阿里巴巴找黄金宝箱I.cpp
@@ -1,7 +1,67 @@
+#include "dbg.h"
+
+#include <cassert>
 #include <iostream>
+#include <numeric>
+#include <sstream>
+#include <string>
+#include <vector>
 
 using namespace std;
 
+int findIndex(const vector<int> &arr) {
+    int leftSum = 0, rightSum = accumulate(arr.begin(), arr.end(), 0) - arr[0];
+    if (leftSum == rightSum) {
+        return 0;
+    }
+    for (int i = 1; i < arr.size(); ++i) {
+        leftSum += arr[i - 1];
+        rightSum -= arr[i];
+        // dbg(leftSum, rightSum);
+        if (leftSum == rightSum) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+void test() {
+    {
+        vector<int> arr{2, 5, -1, 8, 6};
+        int         firstIndex = findIndex(arr);
+        assert(firstIndex == 3);
+    }
+
+    {
+        vector<int> arr{8, 9};
+        int         firstIndex = findIndex(arr);
+        assert(firstIndex == -1);
+    }
+
+    {
+        vector<int> arr{11};
+        int         firstIndex = findIndex(arr);
+        assert(firstIndex == 0);
+    }
+
+    cout << "All test cases passed!" << endl;
+}
+
 int main() {
-    
+    test();
+
+    cout << "Enter the arr" << endl;
+    string line;
+    getline(cin, line);
+    stringstream ss(line);
+
+    vector<int> arr;
+    string      temp;
+    while (getline(ss, temp, ',')) {
+        arr.emplace_back(stoi(temp));
+    }
+
+    cout << "Result: " << findIndex(arr) << endl;
+
+    return 0;
 }
