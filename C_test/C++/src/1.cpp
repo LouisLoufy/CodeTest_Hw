23fa20ed10d307af3f491b8c1a07ce1386a298b2
Louis <1659035538@qq.com>
2023-12-22 22:11:47 +0800

    C_test: 1

--- C_test/C++/src/1.cpp
+++ C_test/C++/src/1.cpp
@@ -1,13 +1,12 @@
 #include <algorithm>
 #include <cassert>
 #include <iostream>
-#include <memory>
 #include <vector>
 
 struct Node {
-    int                   weight;
-    std::shared_ptr<Node> left;
-    std::shared_ptr<Node> right;
+    int   weight;
+    Node *left;
+    Node *right;
 
     Node(int w)
         : weight(w)
@@ -16,39 +15,43 @@ struct Node {
 };
 
 // 构建Huffman树
-std::shared_ptr<Node> huffman_tree(const std::vector<int> &data) {
+Node *huffman_tree(const std::vector<int> &data) {
     if (data.empty()) {
-        return {};
+        return nullptr;
     }
 
-    std::vector<std::shared_ptr<Node>> nodes;
+    // 使用 nodes 接收data的权重
+    std::vector<Node *> nodes;
     for (int weight : data) {
-        nodes.push_back(std::make_shared<Node>(weight));
+        nodes.push_back(new Node(weight));
     }
 
-    while (nodes.size() > 1) {
+    while (nodes.size() > 1) { // 结束判断 直到只剩下一个最后一个节点
+        // 按照权重对 nodes 进行升序排序
         std::sort(nodes.begin(), nodes.end(),
                   [](const auto &a, const auto &b) { return a->weight < b->weight; });
 
-        auto left  = std::move(nodes[0]);
-        auto right = std::move(nodes[1]);
+        // 从 nodes 中移除权重最小的两个节点，分别记为 left 和 right
+        Node *left  = nodes[0];
+        Node *right = nodes[1];
         nodes.erase(nodes.begin(), nodes.begin() + 2);
 
-        auto merged_node   = std::make_shared<Node>(left->weight + right->weight);
-        merged_node->left  = std::move(left);
-        merged_node->right = std::move(right);
-        nodes.push_back(std::move(merged_node));
+        // 创建一个新的节点 merged_node，其权重为left和right节点的权重之和
+        Node *merged_node  = new Node(left->weight + right->weight);
+        merged_node->left  = left;
+        merged_node->right = right;
+        nodes.push_back(merged_node);
     }
 
-    return std::move(nodes[0]);
+    return nodes[0];
 }
 
 // 前序遍历，用于进行 Huffman 树的前序遍历
 void preorder(const Node *root, std::vector<int> &result) {
     if (root) {
-        preorder(root->left.get(), result);
+        preorder(root->left, result);
         result.push_back(root->weight);
-        preorder(root->right.get(), result);
+        preorder(root->right, result);
     }
 }
 
@@ -56,9 +59,9 @@ void test() {
     // 空测试
     {
         std::vector<int> intput = {};
-        auto             root   = huffman_tree(intput);
+        Node            *root   = huffman_tree(intput);
         std::vector<int> result;
-        preorder(root.get(), result);
+        preorder(root, result);
 
         std::vector<int> expected = {};
         assert(result == expected);
@@ -67,9 +70,9 @@ void test() {
     // 单个元素
     {
         std::vector<int> intput = {5};
-        auto             root   = huffman_tree(intput);
+        Node            *root   = huffman_tree(intput);
         std::vector<int> result;
-        preorder(root.get(), result);
+        preorder(root, result);
 
         std::vector<int> expected = {5};
         assert(result == expected);
@@ -78,9 +81,9 @@ void test() {
     // 标准测试
     {
         std::vector<int> intput = {5, 29, 7, 8, 14, 23, 3, 11};
-        auto             root   = huffman_tree(intput);
+        Node            *root   = huffman_tree(intput);
         std::vector<int> result;
-        preorder(root.get(), result);
+        preorder(root, result);
 
         std::vector<int> expected = {3, 8, 5, 19, 11, 42, 23, 100, 29, 58, 14, 29, 7, 15, 8};
         assert(result == expected);
@@ -100,10 +103,10 @@ int main() {
         std::cin >> weight;
     }
 
-    auto root = huffman_tree(input_data);
+    Node *root = huffman_tree(input_data);
 
     std::vector<int> result;
-    preorder(root.get(), result);
+    preorder(root, result);
 
     for (const auto &value : result) {
         std::cout << value << " ";
